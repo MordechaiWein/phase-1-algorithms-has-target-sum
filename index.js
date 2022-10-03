a684464795c1c6f1c1c38e03046f790a9730c36a
Mordechai <mordwein77@gmail.com>
2022-10-03 19:17:21 -0400

    commit tree c061bf8a08e47aeb425caa28036eb483f65f0d9f

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+
+const complement = target - array[i];
+
+for(let j = i + 1; j < array.length; j++){
+//  [3, 8, 12, 4, 11, 7], 10)
+if (array[j] === complement) return true 
+
+}
+
+  }
+  return false
 }
 
 /* 
